f12facda401207eca0b3ad9d67b7199bdb8f1869
Varadendra Deshpande <varadendrad@gmail.com>
2018-10-30 15:01:25 +0530

    Changed to Popular array function. TRANS_RECORD_DATA was working but in case of Vertex array its not working

--- NativeLib/NativeLib.h
+++ NativeLib/NativeLib.h
@@ -1,20 +1,48 @@
 #ifndef _NATIVELIB_H_
 #define _NATIVELIB_H_
-
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL 0
+#else
+#define NULL ((void *)0)
+#endif
+#endif
 #ifndef MYAPI
 #define MYAPI
 #endif
+typedef struct Vertex {
+	float x;
+	float y;
+	float z;
+
+	/// constructor
+	Vertex() : x(0), y(0), z(0) {}
+	Vertex(float x1, float y1, float z1) {
+		x = x1; y = y1; z = z1;
+	}
+
+	~Vertex() {}
+	 
+} Vertex;
+struct  TRANS_RECORD_DATA {
+	int     ThrowItemCount;
+	Vertex* arrayValue;
+	float floatValue;
+	//TRANS_RECORD_DATA(): ThrowItemCount(0), arrayValue(NULL),floatValue(0)
+	//{ 
+
+	//}
+} ;
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-	__declspec(dllexport) const char* Print_line(const char* str);
-
-	struct  TRANS_RECORD_DATA {
-		int     ThrowItemCount;
-	};
-
+	__declspec(dllexport)  void LoadData( );
+	__declspec(dllexport)  void Print_line(char* str);
+	__declspec(dllexport) void Print_Struct(TRANS_RECORD_DATA *str);
+	__declspec(dllexport)  void PopulateArray(Vertex** intptr, int* count);
 #ifdef __cplusplus
 }
 #endif
--- PInvokeTest/Program.cs
+++ PInvokeTest/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace PInvokeTest
 {
@@ -13,15 +15,66 @@ namespace PInvokeTest
 
     public class Printer
     {
-        private string str;
+        private StringBuilder str = new StringBuilder(256);
+        public TRANS_RECORD_DATA strData = new TRANS_RECORD_DATA();
+        IntPtr intptr = new IntPtr();
 
         public void Print()
         {
-            Print_line(ref str);
-            Console.WriteLine(str);
+            LoadData();
+
+            //Print_line(str);
+            //Console.WriteLine(str + "  g    a");
+            //Print_Struct(ref strData);
+            //Console.WriteLine(strData.ThrowItemCount + "  g    a");
+            int count = 0;
+            PopulateArray(ref strData.arrayValue, ref count);
+            float[] resultVertices = new float[count];
+             //Marshal.Copy(intptr, strData, 0, count);
         }
 
         [DllImport("NativeLib.dll")]
-        private static extern string Print_line(ref string str);
+        private static extern void Print_line(StringBuilder str);
+        [DllImport("NativeLib.dll")]
+        private static extern void LoadData( );
+
+        [DllImport("NativeLib.dll")]
+        private static extern void Print_Struct(ref TRANS_RECORD_DATA str);
+
+
+        [DllImport("NativeLib.dll")]
+        private static extern void PopulateArray(ref Vertex[] intptr, ref int count);
+
     }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct TRANS_RECORD_DATA
+    {
+        public int ThrowItemCount1;
+        public  Vertex[]  arrayValue; 
+        public int ThrowItemCount;
+        public float floatValue;
+
+    //   internal TRANS_RECORD_DATA(int a)
+    //    {
+    //        ThrowItemCount1=0;
+    //      arrayValue = new Vertex[12];
+    //    ThrowItemCount =8;
+    //      floatValue= 0.5f;
+    //}
+    };
+     public struct Vertex
+    {
+        float x;
+        float y;
+        float z;
+         
+        ///// constructor 
+        //Vertex(float x1, float y1, float z1)
+        //{
+        //    x = x1; y = y1; z = z1;
+        //}
+
+
+    };
 }
