5a220be245045091a2214c7eb38dca6f3ec6f575
Varadendra Deshpande <varadendrad@gmail.com>
2018-10-30 15:01:25 +0530

    Changed to Popular array function. TRANS_RECORD_DATA was working but in case of Vertex array its not working

--- NativeLib/NativeLib.cpp
+++ NativeLib/NativeLib.cpp
@@ -1,8 +1,50 @@
 #include "NativeLib.h"
 #include <stdio.h>
+#include<string.h>
+#include <iostream>
 
-const char* Print_line(const char* str) {
-	str = "lll";
+TRANS_RECORD_DATA*  stra = new TRANS_RECORD_DATA;
+void Print_line(char* str) { 
+
+	strcpy(str, "Bar");
 	printf("%s\n", str);
-	return str;
+	return;
+} 
+void LoadData()
+{
+	stra->arrayValue = new Vertex[12];
+	stra->floatValue = .552;
+	stra->ThrowItemCount = 34;
+	for (int c = 0; c < 12; c++) {
+		int vertexIndex = c * 3;
+
+		stra->arrayValue[c].x = vertexIndex;
+		stra->arrayValue[c].y = vertexIndex * 2 + 1;
+		stra->arrayValue[c].z = vertexIndex * 6 + 2;
+
+	}
 }
+void Print_Struct(TRANS_RECORD_DATA *str) {
+	str->ThrowItemCount = stra->ThrowItemCount;
+	str->floatValue = stra->floatValue;
+
+	//Marshal.Copy(str, stra, 0, 20);
+
+
+	for (int i = 0; i < 12; i++)
+	{
+		str->arrayValue[i].x = stra->arrayValue[i].x;
+		str->arrayValue[i].y = stra->arrayValue[i].y;
+		str->arrayValue[i].z = stra->arrayValue[i].z;
+	}
+	return;
+}
+	void PopulateArray(Vertex** intptr, int* count)
+	{
+
+		*count = 12;
+		for (int i = 0; i < *count; i++)
+		{
+			*(intptr+i) = &stra->arrayValue[i];
+		} 
+	}
